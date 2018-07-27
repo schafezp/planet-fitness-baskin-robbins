774378bcd9354f9ca3e8ec24771f79ecbc0bd267
Zachary Schafer <schafezp@rose-hulman.edu>
2018-07-26 20:14:15 -0400

    add both pf and br markers to the map

--- server/app/routes.py
+++ server/app/routes.py
@@ -5,7 +5,9 @@ import pandas as pd
 @app.route('/')
 @app.route('/index')
 def index():
-    df = pd.read_csv('../evidence-files/pf-locations-cleaned.csv',sep='\t')
+    pfdf = pd.read_csv('../evidence-files/pf-locations-cleaned.csv',sep='\t')
+    brdf = pd.read_csv('../evidence-files/br-locations-cleaned.csv',sep='\t')
     
-    pfdata  = {'df': df[['lat','lng']].to_json(orient='index').rstrip()}
-    return render_template('index.html', title='Home', pfdata=pfdata)
+    pfdata  = {'df': pfdf[['lat','lng']].to_json(orient='split').rstrip()}
+    brdata  = {'df': brdf[['lat','lng']].to_json(orient='split').rstrip()}
+    return render_template('index.html', title='Home', pfdata=pfdata, brdata=brdata)
